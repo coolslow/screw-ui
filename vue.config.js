a4a167dd662abd608a0b559d87935497a240fab8
mizhon <mizhon@live.cn>
2020-05-25 13:54:21 +0800

    update codes

--- vue.config.js
+++ vue.config.js
@@ -37,5 +37,18 @@ module.exports = {
       .set('vue$', 'vue/dist/vue.esm.js')
       .set('@assets', resolvePath('example/assets'))
       .set('@utils', resolvePath('example/utils'))
+
+    // 删除旧loader，添加新svg loader
+    // 删除的原因是原有的svg是用file-loader解析的，现在改用svg-sprite-loader解析
+    // 注：可以通过vue inspect > output.json 查看loader信息
+    config.module.rules.delete('svg')
+    config.module
+      .rule('svg')
+      .test(/\.svg$/)
+      .use('svg-sprite-loader')
+      .options({
+        name: '[name]-[hash:7]',
+        prefixize: true
+      })
   }
 }
