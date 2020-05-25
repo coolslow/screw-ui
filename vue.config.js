4f295c531f50575f69571d4b1573df09af63d3ca
mizhon <mizhon@live.cn>
2020-05-25 13:42:29 +0800

    update codes

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,41 @@
+'use strict'
+const path = require('path')
+
+// 设置启动页HTML标题
+const title = 'Screw UI'
+// 设置默认端口
+const PORT = process.env.port || 3000
+
+function resolvePath (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   publicPath: './',
+  outputDir: 'dist',
+  assetsDir: 'static',
+  lintOnSave: true,
+  productionSourceMap: false,
   pages: {
     index: {
       entry: 'examples/main.js',
       template: 'public/index.html',
       filename: 'index.html',
-      title: 'Screw UI'
+      title: title
     }
   },
   devServer: {
-    port: 3000
+    open: true,
+    port: PORT,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('vue$', 'vue/dist/vue.esm.js')
+      .set('@assets', resolvePath('example/assets'))
+      .set('@utils', resolvePath('example/utils'))
   }
 }
