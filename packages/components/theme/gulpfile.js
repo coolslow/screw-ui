3939cff53d422bae60f261a0c6de62def036e336
mizhon <mizhon@live.cn>
2020-06-02 12:34:48 +0800

    style(style files): create Style files and add some gulp function

--- packages/components/theme/gulpfile.js
+++ packages/components/theme/gulpfile.js
@@ -4,14 +4,9 @@ const del = require('del')
 const path = require('path')
 const less = require('gulp-less')
 const { series, src, dest } = require('gulp')
-const LessAutoprefix = require('less-plugin-autoprefix')
 const postcss = require('gulp-postcss')
 const cssnano = require('cssnano')
-// const autoprefixer = require('autoprefixer')
-
-var autoprefix = new LessAutoprefix({
-  browsers: ['last 2 versions']
-})
+const autoprefixer = require('autoprefixer')
 
 // 删除
 function clean() {
@@ -24,10 +19,10 @@ function compile() {
     .pipe(less({
       paths: [path.join(__dirname, './src')]
     }))
-    .pipe(less({
-      plugins: [autoprefix]
-    }))
-    .pipe(postcss([/** autoprefixer(), */cssnano()])) // PostCSS plugins 是否考虑使用 PostCSS 统一处理 @mizhon
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano()
+    ])) // PostCSS plugins 是否考虑使用 PostCSS 统一处理 @mizhon
     .pipe(dest('./lib'))
 }
 
