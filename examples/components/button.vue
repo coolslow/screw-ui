2d7ceccd21863f4faab1402c419e1dee93d53d65
mizhon <mizhon@live.cn>
2020-06-09 23:13:55 +0800

    style(style): add omit color for button styles

--- examples/components/button.vue
+++ examples/components/button.vue
@@ -1,11 +1,12 @@
 <template>
-  <div>
+  <div class="button-samples">
     <screw-button @click="handleClick1">按钮一</screw-button>
     <screw-button size="small">按钮二</screw-button>
     <screw-button size="mini">按钮三</screw-button>
-    <screw-button type="success">按钮一</screw-button>
-    <screw-button type="danger" size="small">按钮二</screw-button>
-    <screw-button type="warning" size="mini">按钮三</screw-button>
+    <screw-button type="primary">Primary</screw-button>
+    <screw-button type="success">Success</screw-button>
+    <screw-button type="danger" size="small">Danger</screw-button>
+    <screw-button type="warning" size="mini">Warning</screw-button>
   </div>
 </template>
 <script>
