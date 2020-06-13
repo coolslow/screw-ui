84d4ec912cbcf4dc8583bf4b10f275c2451561bd
mizhon <mizhon@live.cn>
2020-06-13 12:54:54 +0800

    style(style): update style

--- examples/components/button.vue
+++ examples/components/button.vue
@@ -1,12 +1,23 @@
 <template>
   <div class="button-samples">
-    <screw-button @click="handleClick1">按钮一</screw-button>
-    <screw-button size="small">按钮二</screw-button>
-    <screw-button size="mini">按钮三</screw-button>
-    <screw-button type="primary">Primary</screw-button>
-    <screw-button type="success">Success</screw-button>
-    <screw-button type="danger" size="small">Danger</screw-button>
-    <screw-button type="warning" size="mini">Warning</screw-button>
+    <div class="item">
+      <screw-button @click="handleClick1">按钮一</screw-button>
+      <screw-button size="small">按钮二</screw-button>
+      <screw-button size="mini">按钮三</screw-button>
+      <screw-button type="primary">Primary</screw-button>
+      <screw-button type="success">Success</screw-button>
+      <screw-button type="danger" size="small">Danger</screw-button>
+      <screw-button type="warning" size="mini">Warning</screw-button>
+    </div>
+    <div class="item">
+      <screw-button @click="handleClick1" disabled>按钮一</screw-button>
+      <screw-button size="small" disabled>按钮二</screw-button>
+      <screw-button size="mini" disabled>按钮三</screw-button>
+      <screw-button type="primary" disabled>Primary</screw-button>
+      <screw-button type="success" disabled>Success</screw-button>
+      <screw-button type="danger" size="small" disabled>Danger</screw-button>
+      <screw-button type="warning" size="mini" disabled>Warning</screw-button>
+    </div>
   </div>
 </template>
 <script>
@@ -18,3 +29,12 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.button-samples {
+  display: flex;
+  flex-direction: column;
+  .item {
+    margin: 5px 10px;
+  }
+}
+</style>
