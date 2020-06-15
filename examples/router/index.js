6ef749642abb2729d90168ddb3ef78f5e066f091
kingzez <kingzez@sina.com>
2020-06-15 16:58:53 +0800

    feat(example): example router auto import

--- examples/router/index.js
+++ examples/router/index.js
@@ -3,24 +3,27 @@ import VueRouter from 'vue-router'
 
 import Home from '../Home.vue'
 
+const modulesFiles = require.context('../components', false, /\.vue$/)
+
+const modules = modulesFiles.keys().reduce((modules, modulePath) => {
+  const module = modulesFiles(modulePath)
+  const component = module.default
+  // todo optimization
+  const name = /\.*\/(.*\.)/.exec(modulePath)[1].replace(/\./, '')
+  modules.push({
+    path: `/${component.name || name}`,
+    name: component.name || name,
+    component: component
+  })
+  return modules
+}, [])
+
+// console.log(modules)
+
 Vue.use(VueRouter)
 
 export const componentRoutes = [
-  {
-    path: '/button',
-    name: 'button',
-    component: () => import('../components/button.vue')
-  },
-  {
-    path: '/badge',
-    name: 'badge',
-    component: () => import('../components/badge.vue')
-  },
-  {
-    path: '/tabs',
-    name: 'tabs',
-    component: () => import('../components/tabs.vue')
-  }
+  ...modules
 ]
 
 export default new VueRouter({
