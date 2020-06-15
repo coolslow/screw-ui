5567004f4685124579e7a0edf48e693b4c3a8eb3
kingzez <kingzez@sina.com>
2020-06-16 01:00:56 +0800

    feat(example): add avatar example and full import ui

---
+++ examples/components/avatar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="avatar-samples">
+    <div v-for="size in sizeList" :key="size" class="avatars">
+      <screw-avatar :size="size" :src="src"></screw-avatar>
+    </div>
+    <div v-for="shape in shapes" :key="shape" class="avatars">
+      <screw-avatar size="large" :shape="shape" :src="src"></screw-avatar>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      shapes: ['circle', 'square'],
+      sizeList: ['large', 'medium', 'small'],
+      src: 'https://avatars0.githubusercontent.com/u/10891613'
+    }
+  }
+}
+</script>
+<style lang="less" scoped>
+.avatar-samples {
+  display: flex;
+  justify-content: space-between;
+  width: 50%;
+}
+</style>
--- examples/main.js
+++ examples/main.js
@@ -2,12 +2,16 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import { ScrewBadge, ScrewButton } from '../packages/components'
+// import { ScrewBadge, ScrewButton } from '../packages/components'
+import ScrewUI from '../packages/components'
+
 // NOTE: 添加packages组件中最终打包生成的css文件样式
 import '../packages/components/theme/lib/index.css'
 
-Vue.use(ScrewBadge)
-Vue.use(ScrewButton)
+// Vue.use(ScrewBadge)
+// Vue.use(ScrewButton)
+console.log(ScrewUI)
+Vue.use(ScrewUI)
 
 Vue.config.productionTip = false
 
