a626078079fa9afa455052342898939c227f62a6
mizhon <mizhon@live.cn>
2020-06-16 10:41:28 +0800

    docs(example): add badge example

--- examples/components/badge.vue
+++ examples/components/badge.vue
@@ -1,15 +1,35 @@
 <template>
   <div class="badge-samples">
-    <screw-badge is-dot>
-      <screw-button type="primary" @click="handleTestClick">测试</screw-button>
-    </screw-badge>
+    <div class="item">
+      <screw-badge :is-dot="true">
+        <screw-button type="primary" @click="handleTestClick"
+          >测试</screw-button
+        >
+      </screw-badge>
+    </div>
+    <div class="item">
+      <screw-badge value="98" :max="20">
+        <screw-button type="success">测试</screw-button>
+      </screw-badge>
+    </div>
+    <div class="item">
+      <screw-badge :max="20" :value="value1">
+        <screw-button>测试</screw-button>
+      </screw-badge>
+    </div>
+    <div class="item">
+      <screw-badge :value="value2">
+        <screw-button type="warning">测试</screw-button>
+      </screw-badge>
+    </div>
   </div>
 </template>
 <script>
 export default {
   data() {
     return {
-      value: 98
+      value1: 100,
+      value2: 'hot'
     }
   },
   methods: {
@@ -19,3 +39,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.badge-samples {
+  display: flex;
+  flex-direction: row;
+  .item {
+    padding: 10px 20px;
+  }
+}
+</style>
